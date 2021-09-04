7e8c0492dd10c93504301e15e4dc7cd3e6d5ed25
Reimi Matsumoto <reeeeeem2i@gmail.com>
2021-09-04 19:23:04 +0900

    Deployment Target iOS 11.0対応
    * UISceneは13.0-なので使用箇所を削除
    * xcprojファイルが無視されていたので管理対象に追加

--- GitHubUserViewer/Root/RootViewController.swift
+++ GitHubUserViewer/Root/RootViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  RootViewController.swift
 //  GitHubUserViewer
 //
 //  Created by Reimi Matsumoto on 2021/09/04.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class RootViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
-
-
 }
-
